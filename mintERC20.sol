21b3c34137a59e2a667997cd439318cc2d0b2fd7
Errly223093 <lmjgmm@naver.com>
2023-06-18 23:36:01 +0700

    ✨ [feat] mintERC20 생성

---
+++ mintERC20.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+
+contract USDT_TEST is ERC20("US Dollar Tether","USDT") {
+    
+    receive() external payable{}
+    constructor(uint _totalSupply) {
+        _mint(msg.sender, _totalSupply);
+    }
+
+    function getBalance() public view returns(uint) {
+        return balanceOf(msg.sender);
+    }
+
+    function mintToken(uint _amount) public {
+        _mint(address(this), _amount);
+    }
+    
+    function decimals() public pure override returns (uint8) {
+        return 0;
+    }
+}
