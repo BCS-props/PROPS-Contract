69e08369141d7f9a119d5b55162762a1e5aa4997
Errly223093 <lmjgmm@naver.com>
2023-06-28 23:04:32 +0800

    ✨ [feat] Add getTotalVotePower Function

--- governance.sol
+++ governance.sol
@@ -16,7 +16,7 @@ contract governance {
         uint num; // 자동 카운팅
         uint time; // 자동 카운팅 ==> 2주가 지나면 더이상 투표가 불가능. 2주가 지난다면 close 된 것으로 간주.(frontend 에서 처리..)
         address maker; // msg.sender
-            
+
         string subject;
         string summary;
         string method;
@@ -35,6 +35,7 @@ contract governance {
 
     mapping(address => mapping(uint => myStatus)) checkMyStatus; // 내 투표 확인하기
     mapping(address => uint) votePower; // 투표권 갯수
+    uint totalVotePower; // 유저들이 갖고 있는 총 투표권 갯수
     
     address mintNFTContract;
     address admin;
@@ -59,12 +60,14 @@ contract governance {
     function openProposal(string memory _subject, string memory _summary, string memory _method, string memory _conclusion) public {
         require(votePower[msg.sender] >= 1,"At least one voting power is required.");
         votePower[msg.sender] -= 1;
+        totalVotePower -= 1;
         proposals.push( proposal ( ++P_number, block.timestamp, msg.sender,_subject, 
         _summary, _method, _conclusion, 0, 0 ));
     } // 안건 제안. 투표권을 1개 소모하고, 2주가 지나면 자동으로 종료 됨.
 
     function openVotesagree(uint P_numbers) public checkVoteRequire(P_numbers) {
         votePower[msg.sender] -= 1;
+        totalVotePower -= 1;
         proposals[P_numbers].agree++;
         checkMyStatus[msg.sender][P_numbers].count++;
         checkMyStatus[msg.sender][P_numbers].agree++;
@@ -72,14 +75,20 @@ contract governance {
 
     function openVotesdisagree(uint P_numbers) public checkVoteRequire(P_numbers) {
         votePower[msg.sender] -= 1;
+        totalVotePower -= 1;
         proposals[P_numbers].disagree++;
         checkMyStatus[msg.sender][P_numbers].count++;
     } // n번 안건 반대 버튼을 누르면 작동
 
     function increaseVotePower(uint _number, address _address) external {
-        require(mintNFTContract == msg.sender); // 민팅 컨트랙트에서만 호출 가능
+        require(msg.sender == mintNFTContract); // 민팅 컨트랙트에서만 호출 가능
         votePower[_address] += _number;
     } // NFT 구매 시, 투표권 증가시키는 함수. 다른 컨트랙트에서 사용됨.
+
+    function increaseTotalVotePower(uint _number) external {
+        require(msg.sender == mintNFTContract); // 민팅 컨트랙트에서만 호출 가능
+        totalVotePower += _number;
+    }
 
     function userVoteCheck(uint P_numbers) public view returns(myStatus memory){
         return checkMyStatus[msg.sender][P_numbers]; 
@@ -87,19 +96,23 @@ contract governance {
 
     function getVotePower(address msgsender) public view returns(uint){
         return votePower[msgsender]; 
-    } // 유저의 투표권 갯수 값 불러오기
+    } // 유저의 투표권 갯수 반환.
 
     function getP_number() public view returns(uint){
         return P_number; 
-    } // 현재 제안된 안건의 갯수 값 불러오기
+    } // 현재 제안된 안건의 갯수 반환.
 
     function getMyStatus(uint P_numbers) public view returns(uint){
         return checkMyStatus[msg.sender][P_numbers].count; 
-    } // 유저가 n번째 안건에 몇번 투표했는지 값 불러오기
+    } // 유저가 n번째 안건에 몇번 투표했는지 반환.
 
     function getProposal(uint P_numbers) public view returns(proposal memory){
         return proposals[P_numbers-1]; 
     } // 안건번호로 카운팅.
+
+    function getTotalVotePower() public view returns(uint){
+        return totalVotePower;
+    } // 유저들이 소유한 모든 투표수를 반환.
 
     function Test_increasedVotePower() public {
         votePower[msg.sender] += 3;
